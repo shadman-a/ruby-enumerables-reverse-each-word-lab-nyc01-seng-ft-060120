e8c947a57b3883e25ce134f0ac874048b490c2c0
Shadman Ahmed <shadman@outlook.com>
2020-05-22 01:47:28 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+# def reverse_each_word(string)
+#  array = string.split(/ /)
+#  array.each do |word|
+#    word.reverse!
+#  end
+#  array.join(' ')
+# end
+
+def reverse_each_word(string)
+  array = string.split(/ /)
+ array.
+end
